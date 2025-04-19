eef1752a5c8b68b8ade3f124fa4db874dd0cf065
Osvaldo1408exe <osvaldo1408.exe@gmail.com>
2025-04-19 17:36:01 -0300

    feat: implementa MovieRepository com JpaRepository

---
+++ src/main/java/com/osvaldo/gra/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.osvaldo.gra.repository;
+
+import com.osvaldo.gra.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+}
