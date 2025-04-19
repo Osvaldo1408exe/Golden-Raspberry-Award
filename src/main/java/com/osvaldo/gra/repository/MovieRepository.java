7e880e87fded4dab5296e72543e38bdea38a4556
Osvaldo1408exe <osvaldo1408.exe@gmail.com>
2025-04-19 18:28:27 -0300

    feat: retornar produtores com menores e maiores intervalos

--- src/main/java/com/osvaldo/gra/repository/MovieRepository.java
+++ src/main/java/com/osvaldo/gra/repository/MovieRepository.java
@@ -4,6 +4,9 @@ import com.osvaldo.gra.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    List<Movie> findByWinnerIgnoreCase(String yes);
 }
