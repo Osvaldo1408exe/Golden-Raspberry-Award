fe2573e80678a2e01593d805bf66f50f09eba703
Osvaldo1408exe <osvaldo1408.exe@gmail.com>
2025-04-19 20:27:35 -0300

    feat(test): dados genéricos para teste de resposta

---
+++ src/test/resources/teste-data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO movie (movie_year, title, studio, producer, winner) VALUES
+(1999, 'Movie X', 'Studio A', 'Producer 1', 'yes'),
+(2000, 'Movie Y', 'Studio A', 'Producer 2', 'no'),
+(1999, 'Movie X', 'Studio A', 'Producer 1', 'yes'),
+(2000, 'Movie Y', 'Studio A', 'Producer 2', 'yes');
